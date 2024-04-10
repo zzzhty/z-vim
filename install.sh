9573a3870be7450b854739489b96ee60f90aa760
Tianyi Zhang <tyz1024@gmail.com>
2024-04-10 23:35:25 +0800

    install script

--- install.sh
+++ install.sh
@@ -3,36 +3,30 @@
 BASEDIR=$(dirname $0)
 cd $BASEDIR
 CURRENT_DIR=`pwd`
-
-lnif() {
-    if [ -e "$1" ]; then
-        ln -sf "$1" "$2"
-    fi
-}
+TARGET_DIR=$HOME/.vim
 
 echo "Step1: backing up current vim config"
 today=`date +%Y%m%d`
 for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc $HOME/.vimrc.bundles; do [ -e $i ] && [ ! -L $i ] && mv $i $i.$today; done
 for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc $HOME/.vimrc.bundles; do [ -L $i ] && unlink $i ; done
 
-echo "Step2: setting up symlinks"
-lnif $CURRENT_DIR/vimrc $HOME/.vimrc
-lnif $CURRENT_DIR/vimrc.bundles $HOME/.vimrc.bundles
-lnif "$CURRENT_DIR/" "$HOME/.vim"
+echo "Step2: copy configure profiles"
+cp $CURRENT_DIR/vimrc $HOME/.vimrc
+cp $CURRENT_DIR/vimrc.bundles $HOME/.vimrc.bundles
 
 echo "Step3: install vundle"
-if [ ! -e $CURRENT_DIR/bundle/Vundle.vim ]; then
+if [ ! -e $TARGET_DIR/bundle/Vundle.vim ]; then
     echo "Installing Vundle"
-    git clone https://github.com/gmarik/Vundle.vim.git $CURRENT_DIR/bundle/Vundle.vim
+    git clone https://github.com/vundleVim/Vundle.vim.git $TARGET_DIR/bundle/Vundle.vim
 else
     echo "Upgrade Vundle"
-    cd "$HOME/.vim/bundle/Vundle.vim" && git pull origin master
+    cd "$TARGET_DIR/bundle/Vundle.vim" && git pull origin main
 fi
 
 echo "Step4: update/install plugins using Vundle"
 system_shell=$SHELL
 export SHELL="/bin/sh"
-vim -u $HOME/.vimrc.bundles +PluginInstall! +PluginClean +qall
+vim -u $HOME/.vimrc +PluginInstall! +PluginClean +qall
 export SHELL=$system_shell
 
 echo "Completely installed!"
